7bbe286ae3a6610fa576aca4f6cd327e73206585
Alejandro Gómez Anaya <alejandro.gomez@correounivalle.edu.co>
2017-11-26 23:12:58 -0500

    Inicio del proyecto Gestor de tareas

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    public function pruebasAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userRepo = $em->getRepository('BackendBundle:User');
+        $users = $userRepo->findAll();
+
+        var_dump($users);
+        die();
+    }
+
+}
